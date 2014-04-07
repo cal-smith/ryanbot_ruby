111aa6c7150311126fb90d20ce349ef12b055735
Callum Smith <callum1234moo@gmail.com>
2014-04-07 00:25:28 -0400

    updates!

--- ryanbot2.rb
+++ ryanbot2.rb
@@ -1,5 +1,11 @@
 ﻿# encoding: utf-8
 require 'snoo'
+require 'sqlite3'
+
+$db = SQLite3::Database.new( "ryan.db" )
+
+$db.execute("CREATE TABLE IF NOT EXISTS ryan ( id VARCHAR(255) PRIMARY KEY);")
+
 
 $ryan = Snoo::Client.new({:user_agent => "RYANBOT by /u/hansolo669", :username => "ryantipbot", :password => ""})
 
@@ -9,6 +15,8 @@ def nice
 end
 
 def tip(tippet, tipper, id, amount)
+	$db.execute("INSERT INTO ryan (id) VALUES (?)", id)
+	puts "tipping"
 	puts "tipper: " << tipper
 	puts "tippet: " << tippet
 	puts "reply to: " << id
@@ -17,44 +25,46 @@ def tip(tippet, tipper, id, amount)
 	text = "[✓] Accepted: #{tipper} → #{amount[0]} ryan (R#{amount[0]} ryan ryancoins) → #{tippet}"
 	$ryan.comment(text, id)
 	nice
+	puts "sending PM"
+	$ryan.send_pm tippet, "YOU GOT TIPPED #{amount} RYAN COINS!", "YAY FOR YOU!"
+	nice
 end
 
-#$ryan.log_in 'ryantipbot', 'MOOMAN'
-#nice
-
-comments = $ryan.get_comments({:subreddit =>'crispypops'})
-nice
-comments = comments["data"]["children"]
-comments.each do |comment|
-	if /ryantipbot/i.match(comment["data"]["body"])
-		puts "potential tipper: " << comment["data"]["author"]
-		tipper = comment["data"]["author"]
-		amount = comment["data"]["body"]
-		amount = /\d+ ryan/i.match(amount)
-		amount = amount[0].match(/\d+/)
-		puts amount
-		link = comment["data"]["link_id"]
-		link[0..2] = ''
-		puts "link: " << link
-		parent = comment["data"]["parent_id"]
-		parent[0..2] = ''
-		puts "parent: " << parent
-		id = comment["data"]["id"]
-		puts "now to expend a api call to check is ryanbot has replied immedietly below"
-		comments = $ryan.get_comments({:subreddit => 'crispypops', :link_id => link, :comment_id => parent})
-		nice
-		reply = comments[1]["data"]["children"][0]["data"]["replies"]["data"]["children"][0]["author"]
-		puts reply
-		unless reply == "ryantipbot"
-			if link == parent
-				puts "link/selfpost"
-				tippet = comment["data"]["link_author"]
-				tip(tippet, tipper, id, amount)
+while true
+	comments = $ryan.get_comments({:subreddit =>'CrispyPops'})
+	nice
+	comments = comments["data"]["children"]
+	comments.each do |comment|
+		if /\/u\/ryantipbot/i.match(comment["data"]["body"])
+			puts "potential tipper: " << comment["data"]["author"]
+			id = comment["data"]["id"]
+			findid = $db.execute("SELECT * FROM ryan WHERE id = ?", id)
+			if findid.empty?
+				tipper = comment["data"]["author"]
+				amount = comment["data"]["body"]
+				amount = /\d+ ryan/i.match(amount)
+				amount = amount[0].match(/\d+/)
+				puts amount
+				link = comment["data"]["link_id"]
+				link[0..2] = ''
+				puts "link: " << link
+				parent = comment["data"]["parent_id"]
+				parent[0..2] = ''
+				puts "parent: " << parent
+				puts "now to get comments"
+				comments = $ryan.get_comments({:subreddit => 'CrispyPops', :link_id => link, :comment_id => parent})
+				if link == parent
+					puts "link/selfpost"
+					tippet = comment["data"]["link_author"]
+					tip(tippet, tipper, id, amount)
+				else
+					puts "pulling that comment author"
+					tippet = comments[1]["data"]["children"][0]["data"]["author"]
+					puts tippet
+					tip(tippet, tipper, id, amount)
+				end
 			else
-				puts "pulling that comment author"
-				tippet = comments[1]["data"]["children"][0]["data"]["author"]
-				puts tippet
-				tip(tippet, tipper, id, amount)
+				puts "skip!"
 			end
 		end
 	end
